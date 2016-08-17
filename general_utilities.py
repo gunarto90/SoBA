b1a826379b9176786176ea7eff2c5c7564faed05
gunarto90 <gunarto.nctu@gmail.com>
2016-08-17 10:00:54 +0800

    Added write file buffered

--- general_utilities.py
+++ general_utilities.py
@@ -19,3 +19,19 @@ def write_to_file(filename, text, append=True):
     with open(filename, mode) as fw:
         fw.write(str(text) + '\n')
     pass
+
+def write_to_file_buffered(filename, text_list, append=True):
+    buffer_size = 10000
+    counter = 0
+    temp_str = ""
+    for text in text_list:
+        if counter < buffer_size:
+            temp_str = temp_str + text + '\n'
+        else:
+            write_to_file(filename, temp_str, append)
+            temp_str = ""
+            counter = 0
+        counter += 1
+    # Write remaining text
+    if temp_str != "":
+        write_to_file(filename, temp_str, append)
