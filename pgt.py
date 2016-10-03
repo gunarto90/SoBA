832269b949c7cf1d45e65c9efdd5923f540b30e2
gunarto90 <gunarto.nctu@gmail.com>
2016-10-03 15:39:32 +0800

    Initial file

---
+++ pgt.py
@@ -0,0 +1,19 @@
+from general_utilities import *
+from base import *
+from classes import *
+
+co_raw_filename = 'co_raw_p{}_k{}_t{}_d{}.csv'
+
+def pgt_personal():
+    pass
+
+def pgt_global():
+    pass
+
+def pgt_temporal():
+    pass
+
+# Main function
+if __name__ == '__main__':
+    debug('PGT start')
+    debug('PGT finished')
