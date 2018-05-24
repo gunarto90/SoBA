d693af4394b07b4588c2fcb7ba61fa005fba0452
gunarto90 <gunarto.nctu@gmail.com>
2018-05-24 20:51:23 +0800

    Beautify colocation code and improve efficiency

--- methods/colocation.py
+++ methods/colocation.py
@@ -7,53 +7,14 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+import math
 ### KD Tree
 from scipy import spatial
 ### Setup Directories for local library
 PWD = os.getcwd()
 sys.path.append(PWD)
 ### Local library
-from common.functions import IS_DEBUG, fn_timer, debug
-
-"""
-KD-Tree functions
-"""
-def intersect(hrect, r2, centroid):
-    """
-    checks if the hyperrectangle hrect intersects with the
-    hypersphere defined by centroid and r2
-    """
-    maxval = hrect[1,:]
-    minval = hrect[0,:]
-    p = centroid.copy()
-    idx = p < minval
-    p[idx] = minval[idx]
-    idx = p > maxval
-    p[idx] = maxval[idx]
-    return ((p-centroid)**2).sum() < r2
-
-def radius_search(tree, datapoint, radius):
-  """ find all points within radius of datapoint """
-  stack = [tree[0]]
-  inside = []
-  while stack:
-    leaf_idx, leaf_data, left_hrect, \
-              right_hrect, left, right = stack.pop()
-    # leaf
-    if leaf_idx is not None:
-      param=leaf_data.shape[0]
-      distance = np.sqrt(((leaf_data - datapoint.reshape((param,1)))**2).sum(axis=0))
-      near = np.where(distance<=radius)
-      if len(near[0]):
-        idx = leaf_idx[near]
-        distance = distance[near]
-        inside += (zip(distance, idx))
-  else:
-    if intersect(left_hrect, radius, datapoint):
-      stack.append(tree[left])
-    if intersect(right_hrect, radius, datapoint):
-      stack.append(tree[right])
-  return inside
+from common.functions import IS_DEBUG, fn_timer, debug, haversine
 
 """
 Utility functions
@@ -61,7 +22,7 @@ Utility functions
 def extract_geometry(df):
   x = df['latitude'].values
   y = df['longitude'].values
-  data = zip(x.ravel(), y.ravel())
+  data = np.array(zip(x.ravel(), y.ravel()))
   return data
 
 def create_spatial_kd_tree(df):
@@ -85,6 +46,12 @@ def normalize_radius_index(idx):
 
 """
 Co-location generation
+Input:
+- checkins_per_user (dictionary of [int, dataframe])
+- start : the beginning index of co-location generation process (useful for parallelization)
+- finish : the ending index of co-location generation process (useful for parallelization)
+- t_diff : time threshold for co-location criterion (in seconds)
+- s_diff : spatial threshold for co-location criterion (in lat/lon degree)
 """
 @fn_timer
 def generate_colocation(checkins_per_user, start=0, finish=0, t_diff=1800, s_diff=0.001):
@@ -92,35 +59,30 @@ def generate_colocation(checkins_per_user, start=0, finish=0, t_diff=1800, s_dif
   uids = checkins_per_user.keys()
   for i in range(start, finish):
     u_i = uids[i]
-    df_i = checkins_per_user[u_i]
-    debug('User', u_i, len(df_i))
-    s_tree = create_spatial_kd_tree(df_i)
-    t_tree = create_temporal_kd_tree(df_i)
+    df_i = checkins_per_user[u_i].sort_values(by=['timestamp'])
+    debug('#Checkins of User', u_i, ':', len(df_i))
+    si_tree = create_spatial_kd_tree(df_i)
+    ti_tree = create_temporal_kd_tree(df_i)
     for j in range(i+1, len(uids)):
       u_j = uids[j]
-      df_j = checkins_per_user[u_j]
-      geo_j = extract_geometry(df_j)
-      time_j = zip(df_j['timestamp'].values.ravel())
+      df_j = checkins_per_user[u_j].sort_values(by=['timestamp'])
+      sj_tree = create_spatial_kd_tree(df_j)
+      tj_tree = create_temporal_kd_tree(df_j)
       ### temporal co-occurrence
-      t_idx = t_tree.query_ball_point(time_j, t_diff) ### Using temporal distance
-      t_idx = normalize_radius_index(t_idx)
-      # debug('t_idx', t_idx)
-      # t_results, t_count = extract_radius_search_results(u_i, u_j, df_i, df_j, t_idx)
-      t_count = sum(len(x) for x in t_idx.values())
-      ### spatial co-occurrence
+      t_idx = ti_tree.query_ball_tree(tj_tree, t_diff)
+      t_count = sum(len(x) for x in t_idx)
       if t_count > 0:
-        s_idx = s_tree.query_ball_point(geo_j, s_diff)  ### Using spatial distance  ### Problem: Why we do not use the "haversine formula"
-        s_idx = normalize_radius_index(s_idx)
-        # debug('s_idx', s_idx)
-        # results, count = extract_radius_search_results(u_i, u_j, df_i, df_j, s_idx)
-
-        for k in s_idx.keys():
-          temp = set(s_idx[k]).intersection(t_idx[k])
-          if len(temp) > 0:
-            debug(i, j, k, len(temp), temp)
+        # debug(t_count,t_idx)
+        ### spatial co-occurrence
+        s_idx = si_tree.query_ball_tree(sj_tree, s_diff)
+        s_count = sum(len(x) for x in s_idx)
+        ### Only if both temporal and spatial co-occurrence > 0
+        if s_count > 0:
+          ### Finding the intersection and adding colocations to the list
+          colocations.extend(extract_radius_search_results(u_i, u_j, df_i, df_j, s_idx, t_idx))
       ### For debugging purpose
-      # if IS_DEBUG is True and j > 10:
-      #   break
+      if IS_DEBUG is True and j > 300:
+        break
   return colocations
 
 """
@@ -129,19 +91,24 @@ u_i     : User i's ID
 u_j     : User j's ID
 df_i    : Dataframe of user i
 df_j    : Dataframe of user j
-idx     : List of list of all locations within the "radius"
+s_idx   : List of list of all locations within the "radius"
+t_idx   : 
 """
-def extract_radius_search_results(u_i, u_j, df_i, df_j, idx):
+def extract_radius_search_results(u_i, u_j, df_i, df_j, s_idx, t_idx):
   results = []
-  count = 0
-  geometry_i = pd.DataFrame(df_i, columns=['latitude', 'longitude', 'timestamp']).values
-  geometry_j = pd.DataFrame(df_j, columns=['latitude', 'longitude', 'timestamp']).values
-  for j, x in idx.items():
-    count += len(x)
-    if len(x) > 0:
-      for i in x:
-        temp = (u_i, u_j, geometry_i[i], geometry_j[j])
-        results.append(temp)
-  del geometry_i
-  del geometry_j
-  return results, count
+  if len(s_idx) != len(t_idx):
+    return None
+  for i in range(len(s_idx)):
+    temp = set(s_idx[i]).intersection(t_idx[i])
+    for j in temp:
+      di = df_i.iloc[i]
+      dj = df_j.iloc[j]
+      ### Format: user1,user2,location1,location2,time1,time2,lat1,lon1,lat2,lon2,t_diff,s_diff
+      t_diff = math.fabs(di['timestamp'] - dj['timestamp'])
+      s_diff = haversine(di['latitude'], di['longitude'], dj['latitude'], dj['longitude'])
+      obj = (u_i, u_j, int(di['location']), int(dj['location']), di['timestamp'],dj['timestamp'],
+        di['latitude'], di['longitude'], dj['latitude'], dj['longitude'],
+        t_diff, s_diff
+      )
+      results.append(obj)
+  return results
