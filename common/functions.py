0b79d74ce9d01aa1331616ea052916b4e76f0de5
gunarto90 <gunarto.nctu@gmail.com>
2018-05-28 15:53:03 +0800

    Fixed minor bugs

--- common/functions.py
+++ common/functions.py
@@ -69,8 +69,8 @@ def debug(*argv):
 
 def report_progress(counter, start, finish, context='', every_n=100):
   if counter % every_n == 0:
-    debug('Processing {} of {} {} ({:.3f}%) '.format(
-      counter, (finish-start), context, float(counter)*100.0/(finish-start))
+    debug('Processing {} of {} {} ({:.3f}% [{} of {}]) '.format(
+      counter, (finish-start), context, float(counter)*100.0/(finish-start), counter, finish),
     )
  
 def fn_timer(function):
